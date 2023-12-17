ec54ad61dabe5e2de0b809356e3afaa2512e76f0
HuyTran0996 <tranviquanghuy@gmail.com>
2023-12-17 22:48:29 +0700

    2023-12-17

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import express from "express";
-import { engine } from "express-handlebars";
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+
 const app = express();
 const port = 3000;
 
+//HTTP logger
 app.use(morgan("combined"));
 
+//Template engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
 });
 
 app.listen(port, () => {
